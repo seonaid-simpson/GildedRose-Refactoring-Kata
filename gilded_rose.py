ce9ba2d31605e0061a970b1e4a0651bc80f2fc4f
Seonaid Simpson <seonaid.simpson20@gmail.com>
2025-07-10 08:32:13 +0100

    Refactored update_quality() using elif structure to update each item logic separately - this will make it simpler to add new items in future

---
+++ gilded_rose.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            if item.name == "Aged Brie":
+                self.update_aged_brie(item)
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                continue #legendary item, never has to be sold or decreases in quality, do nothing
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.update_backstage_pass(item)
+            elif item.name.startswith("Conjured"):
+                self.update_conjured_item(item)
+            else:
+                self.update_normal_item(item)
+
+    def update_aged_brie(self,item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+    def update_backstage_pass(self,item):
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0;
+            item.quality = 0
+
+    def update_normal_item(self,item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
